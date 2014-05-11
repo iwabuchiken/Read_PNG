50b3f2b565d2e587ead529f40da3bfadd3b33539
ubuntu_12-10 <iwabuchik@ubuntu.(none)>
2014-05-11 00:30:47 -0700

    m
    v-1.0
    ---
    Initial

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: iwabuchik
+ *
+ * Created on May 11, 2014, 12:29 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
